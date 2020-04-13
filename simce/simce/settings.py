2248f11835128edfb0ac56714b9342202c3b53fa
Isidro López <isidro.lopez@mejoredu.gob.mx>
2020-04-13 18:41:19 -0500

    se configura carpeta static, se agrega api D3 y se agrega ejercicio para desplegar gráfico de barras apliladas en el archivo datos_escuela.html

--- simce/simce/settings.py
+++ simce/simce/settings.py
@@ -150,9 +150,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
+
+STATICFILES_DIRS = [
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(BASE_DIR,'static'),
-)
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
